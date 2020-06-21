81721b2a181cd97c78a802a93a90eb639e987bfb
diogosimao <diogosimao@gmail.com>
2020-06-21 16:01:22 -0300

    :thinking: Fixes syntax error

--- travis.sql
+++ travis.sql
@@ -2,6 +2,6 @@
 CREATE USER 'django'@'localhost' IDENTIFIED BY '0gn4jd';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'django'@'localhost';
 # Create DB
-CREATE DATABASE IF NOT EXISTS 'django' DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE 'django';
+CREATE DATABASE IF NOT EXISTS `django` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `django`;
 # Add Data
