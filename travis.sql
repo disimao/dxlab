884586d42bc9baf80da0a830639e1174b0c9415d
diogosimao <diogosimao@gmail.com>
2020-06-21 16:07:16 -0300

    Grants all permissions for database application' user

--- travis.sql
+++ travis.sql
@@ -1,6 +1,6 @@
 # Create Testuser
 CREATE USER 'django'@'localhost' IDENTIFIED BY '0gn4jd';
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'django'@'localhost';
+GRANT ALL ON *.* TO 'django'@'localhost';
 # Create DB
 CREATE DATABASE IF NOT EXISTS `django` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `django`;
